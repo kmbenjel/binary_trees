228cffba30ff17c8b38ca07032e05aa8f15a0e17
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-04 01:26:26 +0100

    edit

--- binary_trees.h
+++ binary_trees.h
@@ -18,6 +18,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
