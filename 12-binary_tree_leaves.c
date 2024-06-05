7b8014637dd1faa69ff69343d4d6588bb9da4d12
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-05 23:55:23 +0100

    edit

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Count leaves
+ * @tree: The root of a binary tree
+ * Return: An integer or NULL
+ */
+
+size_t	binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t	leaves;
+
+	leaves = 0;
+	if (tree)
+	{
+		leaves += (!tree->left && !tree->right) ? 1 : 0;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
+}
