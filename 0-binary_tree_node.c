4259bb543454666952ebc1376a161159bc47b8a3
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-05 18:19:31 +0100

    edit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,5 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node->parent = parent;
 	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	return (new_node);
 }
