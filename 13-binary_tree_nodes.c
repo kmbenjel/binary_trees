81ceea1ece2eb213209b78298036c440ea32ba15
Beellal <mahamanehabiboubilal999@gmail.com>
2024-06-06 07:04:58 -0700

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -13,16 +11,18 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t node_1_child = 0;
 
-	if (!tree)
+	size_t  node = 0;
+
+	if (tree == NULL)
+	{
 		return (0);
-
-	if (tree->right != NULL || tree->left != NULL)
-		(node_1_child += 1);
-
-	node_1_child += binary_tree_nodes(tree->left);
-	node_1_child += binary_tree_nodes(tree->right);
-	return (node_1_child);
-
+	}
+	else
+	{
+		node += ((tree->left || tree->right) ? 1 : 0);
+		node += binary_tree_nodes(tree->left);
+		node += binary_tree_nodes(tree->right);
+		return (node);
+	}
 }
